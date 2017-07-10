71d8ab6f54fd6602a92f7a966360f44b1c44cb5a
fine-day <chenbing_v@163.com>
2017-07-10 11:21:51 +0800

    Update comment_crawler.py
    change something about privacy

--- douban_comments/comment_crawler.py
+++ douban_comments/comment_crawler.py
@@ -18,7 +18,7 @@ page_headers = {
     'Connection': 'keep-alive',
     'Host': 'movie.douban.com',
     'Referer': 'https://movie.douban.com/subject/26411410/',
-    'Cookie': 'bid=TW9FfFM6rLw; gr_user_id=29061151-bb6d-44cd-ad35-cfa8c44fad9e; ct=y; ps=y; ll="118172"; viewed="26274202_26838921_4826033_26468916_3112503_4889838_6789516_26320485"; _pk_ref.100001.4cf6=%5B%22%22%2C%22%22%2C1499574237%2C%22https%3A%2F%2Fwww.douban.com%2Faccounts%2Flogin%3Fredir%3Dhttps%253A%252F%252Fmovie.douban.com%252F%22%5D; _vwo_uuid_v2=49BFB964A55588402302EF90DE89BC30|1595871f7f79336105d5709a8b32edf3; _pk_id.100001.4cf6=4e34811c463e9043.1497948489.28.1499575920.1499570690.; _pk_ses.100001.4cf6=*; __utma=223695111.1940242653.1497948489.1499567326.1499574237.31; __utmb=223695111.0.10.1499574237; __utmc=223695111; __utmz=223695111.1499574237.31.22.utmcsr=douban.com|utmccn=(referral)|utmcmd=referral|utmcct=/accounts/login; __utmt=1; ue="1334968325@qq.com"; dbcl2="146515721:hbPy7u6w894"; ck=HKfg; ap=1; push_noty_num=0; push_doumail_num=0; __utma=30149280.637523572.1496852261.1499574237.1499574278.48; __utmb=30149280.21.10.1499574278; __utmc=30149280; __utmz=30149280.1499574278.48.33.utmcsr=baidu|utmccn=(organic)|utmcmd=organic; __utmv=30149280.14651',
+    'Cookie': '',  # 设置自己的Cookie, 也可以用模拟登陆，然后保存Cookie .
     'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0.1; Nexus 5X Build/MMB29P) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.96 Mobile Safari/537.36 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)'
 }
 
